d8dfd03ed254c2396c695f60a1c01a1b8da17bb0
Joao Neto <joao.fnnt@gmail.com>
2023-06-22 20:26:52 -0300

    End point criado

---
+++ src/main/java/com/educandoweb/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.educandoweb.curso.resources;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.curso.entidades.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Inado", "inado@gmail.com", "999999", "12345");		
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
